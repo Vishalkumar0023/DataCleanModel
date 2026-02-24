0344c955713f0846d89d4fbc797718a34cf5892e
Vishal Kumar <vishal.kumar.connect0@gmail.com>
2026-02-24 21:29:28 +0530

    Fix: prefer binary wheels for faster builds

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 set -o errexit  # exit on error
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install --prefer-binary -r requirements.txt
 
 # Initialize the database
 python -c "
